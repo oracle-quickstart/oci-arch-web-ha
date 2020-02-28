4b7c72beb2f24a7d08cd18e1f00c408a92fb192f
Flavio Pereira <flavio.pereira@oracle.com>
2020-02-28 15:10:15 +0000

    added license

--- scripts/db1.sql
+++ scripts/db1.sql
@@ -1,3 +1,7 @@
+/*Copyright © 2020, Oracle and/or its affiliates. 
+ All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+ */
+ 
 alter session set "_ORACLE_SCRIPT"=true; 
 
 CREATE USER dbfirst IDENTIFIED BY "DevOps_123#"
--- scripts/setup_db1.sh
+++ scripts/setup_db1.sh
@@ -1,2 +1,6 @@
+## Copyright © 2020, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+#!/bin/bash
 sudo bash -c "mv /tmp/db1.sql /home/oracle/ && chmod 777 /home/oracle/db1.sql"
 sudo su - oracle -c "export ORACLE_SID=aTFdb && export ORAENV_ASK=NO && . /usr/local/bin/oraenv && exit | sqlplus / as sysdba @/home/oracle/db1.sql"
