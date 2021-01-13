da1f943d20c3caf9485648426f6af0033e7a81c9
lfeldman <lukasz.feldman@oracle.com>
2021-01-13 12:57:35 +0100

    Fixed oci-arch-web-ha.zip

--- nsg.tf
+++ nsg.tf
@@ -2,7 +2,6 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 # ATPSecurityGroup
-
 resource "oci_core_network_security_group" "ATPSecurityGroup" {
     compartment_id = var.compartment_ocid
     display_name = "ATPSecurityGroup"
@@ -10,25 +9,19 @@ resource "oci_core_network_security_group" "ATPSecurityGroup" {
 }
 
 # Rules related to ATPSecurityGroup
-
 # EGRESS
-
 resource "oci_core_network_security_group_security_rule" "ATPSecurityEgressGroupRule" {
     network_security_group_id = oci_core_network_security_group.ATPSecurityGroup.id
     direction = "EGRESS"
     protocol = "6"
-#    destination = "10.0.0.0/16"
     destination = "10.0.1.0/24"
     destination_type = "CIDR_BLOCK"
 }
-
 # INGRESS
-
 resource "oci_core_network_security_group_security_rule" "ATPSecurityIngressGroupRules" {
     network_security_group_id = oci_core_network_security_group.ATPSecurityGroup.id
     direction = "INGRESS"
     protocol = "6"
-#    source = "10.0.0.0/16"
     source = "10.0.1.0/24"
     source_type = "CIDR_BLOCK"
     tcp_options {
@@ -40,17 +33,13 @@ resource "oci_core_network_security_group_security_rule" "ATPSecurityIngressGrou
 }
 
 # WebSecurityGroup
-
 resource "oci_core_network_security_group" "WebSecurityGroup" {
     compartment_id = var.compartment_ocid
     display_name = "WebSecurityGroup"
     vcn_id = oci_core_virtual_network.vcn.id
 }
-
 # Rules related to WebSecurityGroup
-
 # EGRESS
-
 resource "oci_core_network_security_group_security_rule" "WebSecurityEgressATPGroupRule" {
     network_security_group_id = oci_core_network_security_group.WebSecurityGroup.id
     direction = "EGRESS"
@@ -58,24 +47,19 @@ resource "oci_core_network_security_group_security_rule" "WebSecurityEgressATPGr
     destination = oci_core_network_security_group.ATPSecurityGroup.id 
     destination_type = "NETWORK_SECURITY_GROUP"
 }
-
 resource "oci_core_network_security_group_security_rule" "WebSecurityEgressInternetGroupRule" {
     network_security_group_id = oci_core_network_security_group.WebSecurityGroup.id
     direction = "EGRESS"
     protocol = "6"
     destination = "10.0.0.0/24"
-#    destination = "0.0.0.0/0"
     destination_type = "CIDR_BLOCK"
 }
-
 # INGRESS
-
 resource "oci_core_network_security_group_security_rule" "WebSecurityIngressGroupRules" {
     network_security_group_id = oci_core_network_security_group.WebSecurityGroup.id
     direction = "INGRESS"
     protocol = "6"
     source = "10.0.0.0/24"
- #   source = "0.0.0.0/0"
     source_type = "CIDR_BLOCK"
     tcp_options {
         destination_port_range {
@@ -86,17 +70,13 @@ resource "oci_core_network_security_group_security_rule" "WebSecurityIngressGrou
 }
 
 # LBSecurityGroup
-
 resource "oci_core_network_security_group" "LBSecurityGroup" {
     compartment_id = var.compartment_ocid
     display_name = "LBSecurityGroup"
     vcn_id = oci_core_virtual_network.vcn.id
 }
-
 # Rules related to LBSecurityGroup
-
 # EGRESS
-
 resource "oci_core_network_security_group_security_rule" "LBSecurityEgressInternetGroupRule" {
     network_security_group_id = oci_core_network_security_group.LBSecurityGroup.id
     direction = "EGRESS"
@@ -104,9 +84,7 @@ resource "oci_core_network_security_group_security_rule" "LBSecurityEgressIntern
     destination = "0.0.0.0/0"
     destination_type = "CIDR_BLOCK"
 }
-
 # INGRESS
-
 resource "oci_core_network_security_group_security_rule" "LBSecurityIngressGroupRules" {
     network_security_group_id = oci_core_network_security_group.LBSecurityGroup.id
     direction = "INGRESS"
@@ -122,17 +100,13 @@ resource "oci_core_network_security_group_security_rule" "LBSecurityIngressGroup
 }
 
 # SSHSecurityGroup
-
 resource "oci_core_network_security_group" "SSHSecurityGroup" {
     compartment_id = var.compartment_ocid
     display_name = "SSHSecurityGroup"
     vcn_id = oci_core_virtual_network.vcn.id
 }
-
 # Rules related to SSHSecurityGroup
-
 # EGRESS
-
 resource "oci_core_network_security_group_security_rule" "SSHSecurityEgressGroupRule" {
     network_security_group_id = oci_core_network_security_group.SSHSecurityGroup.id
     direction = "EGRESS"
@@ -140,9 +114,7 @@ resource "oci_core_network_security_group_security_rule" "SSHSecurityEgressGroup
     destination = "0.0.0.0/0"
     destination_type = "CIDR_BLOCK"
 }
-
 # INGRESS
-
 resource "oci_core_network_security_group_security_rule" "SSHSecurityIngressGroupRules" {
     network_security_group_id = oci_core_network_security_group.SSHSecurityGroup.id
     direction = "INGRESS"
