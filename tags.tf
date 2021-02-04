626b3d9ebe5a31c222a0f7a5a4e70ae4e547cab3
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-02-04 17:15:42 +0100

    Introduced sleep for apply and destroy of tag/tagnamespace

--- tags.tf
+++ tags.tf
@@ -2,7 +2,12 @@ resource "oci_identity_tag_namespace" "ArchitectureCenterTagNamespace" {
     compartment_id = var.compartment_ocid
     description = "ArchitectureCenterTagNamespace"
     name = "ArchitectureCenter\\ha-web-app"
+  
+    provisioner "local-exec" {
+       command = "sleep 10"
+    }
 }
+
 resource "oci_identity_tag" "ArchitectureCenterTag" {
     description = "ArchitectureCenterTag"
     name = "release"
@@ -11,5 +16,15 @@ resource "oci_identity_tag" "ArchitectureCenterTag" {
     validator {
         validator_type = "ENUM"
         values         = ["release", "1.0"]
+    }
+
+    provisioner "local-exec" {
+       command = "sleep 20"
+    }
+
+    # Required for longop of tag deletion in background 
+    provisioner "local-exec" {
+       when    = destroy
+       command = "sleep 1200"
     }
 }
