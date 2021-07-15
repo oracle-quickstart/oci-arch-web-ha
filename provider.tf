bed1db5ca4461a20b8c9ebe0d413d5880a63e2a0
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-07-15 13:30:40 +0200

    Updated files for 1.2

--- provider.tf
+++ provider.tf
@@ -2,30 +2,30 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 provider "oci" {
-  region               = var.region
-  tenancy_ocid         = var.tenancy_ocid
-  
+  region       = var.region
+  tenancy_ocid = var.tenancy_ocid
+
   #### BEGIN COMMENT OUT FOR USAGE ON ORM AND/OR CLOUD SHELL
   user_ocid            = var.user_ocid
   fingerprint          = var.fingerprint
   private_key          = local.private_key
   private_key_password = var.private_key_password
   #### END COMMENT OUT FOR USAGE ON ORM AND/OR CLOUD SHELL
-  
+
   disable_auto_retries = "true"
 }
 
 provider "oci" {
-  alias                = "homeregion"
-  region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
-  tenancy_ocid         = var.tenancy_ocid
-  
+  alias        = "homeregion"
+  region       = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
+  tenancy_ocid = var.tenancy_ocid
+
   #### BEGIN COMMENT OUT FOR USAGE ON ORM AND/OR CLOUD SHELL
   user_ocid            = var.user_ocid
   fingerprint          = var.fingerprint
   private_key          = local.private_key
   private_key_password = var.private_key_password
   #### END COMMENT OUT FOR USAGE ON ORM AND/OR CLOUD SHELL
-  
+
   disable_auto_retries = "true"
 }
--- variables.tf
+++ variables.tf
@@ -131,7 +131,9 @@ locals {
   # Dictionary Locals
   compute_flexible_shapes = [
     "VM.Standard.E3.Flex",
-    "VM.Standard.E4.Flex"
+    "VM.Standard.E4.Flex",
+    "VM.Optimized3.Flex",
+    "VM.Standard.A1.Flex"
   ]
   # Checks if is using Flexible Compute Shapes
   is_flexible_node_shape = contains(local.compute_flexible_shapes, var.instance_shape)
